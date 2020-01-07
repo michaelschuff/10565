a73cf557cb552611d8f04c121f8d3ae891cec85e
Joshua Bromley <joshua@mattbromley.com>
2020-01-06 19:06:35 -0800

    Changes

--- skystone/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SkyStoneFinderTest.java
+++ skystone/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SkyStoneFinderTest.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.Autonomous.SkyStoneFinder;
 import org.firstinspires.ftc.teamcode.util.VuforiaLib_Skystone;
 
+
+@Autonomous(name="Test: SkystoneTest", group = "Auto")
 public class SkyStoneFinderTest extends OpMode {
 
     VuforiaLib_Skystone mvLib;
